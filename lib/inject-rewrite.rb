04a652c2149a691ccdfbfd286828dd2238e02466
Richard Lake <ralake1985@gmail.com>
2014-11-30 17:44:20 +0000

    finished

--- lib/inject-rewrite.rb
+++ lib/inject-rewrite.rb
@@ -1,38 +1,21 @@
 class Array
 
-  def my_inject(memo = nil)
+  def my_inject(memo = nil, &block)
     if memo.nil? then memo = self.first
-      self.drop(1).each { |item| memo = yield(memo, item) }
-      memo
+      memo_is_nil(memo, &block)
     else
-      self.each { |item| memo = yield(memo, item) }
-      memo
+      memo_not_nil(memo, &block)
     end
   end
 
+  def memo_is_nil(memo, &block)
+    self.drop(1).each { |item| memo = block.call(memo, item) }
+    memo
+  end
+
+  def memo_not_nil(memo, &block)
+    self.each { |item| memo = block.call(memo, item) }
+    memo
+  end
+
 end
-
-# class Array
-
-#   def my_inject(memo = nil, &block)
-#     if memo.nil? then memo_nil(memo, &block)
-#     else
-#       memo_not_nil(memo, &block)
-#     end
-#   end
-
-  
-#   def memo_nil(memo, &block)
-#     accumulator = Proc.new { |item| memo = block.call(memo, item) }
-#     memo = self.first
-#     self.drop(1).each &accumulator
-#     memo
-#   end
-
-#   def memo_not_nil(memo, &block)
-#     accumulator = Proc.new { |item| memo = block.call(memo, item) }
-#     self.each &accumulator
-#     memo
-#   end
-
-# end
